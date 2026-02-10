0a47e2259446094623517632129086e4b883d903
KavyaAchanta <kavyaachanta2004@gmail.com>
2026-02-10 13:19:12 +0530

    added health check route

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.status(200).send("Server is alive 🚀");
+});
+
 app.use("/contact", contactRoutes);
+
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
